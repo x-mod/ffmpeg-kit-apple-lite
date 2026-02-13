1fb8603b7669033ccb7114e8236de28b40e92fb9
liujianping <gitdig.com@gmail.com>
2026-02-13 16:21:07 +0800

    update scripts

--- Scripts/build-ios.sh
+++ Scripts/build-ios.sh
@@ -1,77 +1,74 @@
 #!/bin/bash
 set -e
 
-FFMPEG_VERSION=6.0
-
-git clone --depth 1 --branch v${FFMPEG_VERSION} https://github.com/arthenica/ffmpeg-kit.git
+git clone --depth 1 --branch v6.0 https://github.com/arthenica/ffmpeg-kit.git
 cd ffmpeg-kit
 
-./ios.sh all \
+export FFMPEG_CONFIGURE_OPTIONS="\
+--disable-everything \
+--enable-small \
+--disable-programs \
+--disable-doc \
+--disable-debug \
+--disable-network \
+--disable-autodetect \
+\
+--enable-avcodec \
+--enable-avformat \
+--enable-avutil \
+--enable-swresample \
+--enable-swscale \
+\
+--enable-protocol=file \
+\
+--enable-demuxer=mov \
+--enable-demuxer=matroska \
+--enable-demuxer=avi \
+--enable-demuxer=flv \
+--enable-demuxer=webm \
+--enable-demuxer=mpegts \
+--enable-demuxer=mpegps \
+--enable-demuxer=asf \
+\
+--enable-muxer=mp4 \
+--enable-muxer=mov \
+--enable-muxer=adts \
+\
+--enable-decoder=h264 \
+--enable-decoder=hevc \
+--enable-decoder=mpeg4 \
+--enable-decoder=mpeg2video \
+--enable-decoder=vp8 \
+--enable-decoder=vp9 \
+--enable-decoder=wmv3 \
+--enable-decoder=msmpeg4v3 \
+\
+--enable-decoder=aac \
+--enable-decoder=mp3 \
+--enable-decoder=ac3 \
+--enable-decoder=pcm_s16le \
+--enable-decoder=vorbis \
+\
+--enable-parser=h264 \
+--enable-parser=hevc \
+--enable-parser=mpeg4video \
+--enable-parser=mpegaudio \
+--enable-parser=aac \
+\
+--enable-encoder=h264_videotoolbox \
+--enable-encoder=aac \
+\
+--enable-hwaccel=h264_videotoolbox \
+"
+
+./ios.sh \
+  --xcframework \
+  --enable-ios-videotoolbox \
+  --enable-ios-audiotoolbox \
+  --enable-ios-zlib \
   --disable-armv7 \
   --disable-armv7s \
-  \
-  --disable-gpl \
-  --disable-nonfree \
-  --disable-debug \
-  --disable-programs \
-  --disable-doc \
-  --disable-avdevice \
-  --disable-postproc \
-  --disable-network \
-  --disable-autodetect \
-  --disable-everything \
-  \
-  --enable-avcodec \
-  --enable-avformat \
-  --enable-avutil \
-  --enable-swresample \
-  --enable-swscale \
-  \
-  --enable-protocol=file \
-  \
-  --enable-demuxer=mov \
-  --enable-demuxer=matroska \
-  --enable-demuxer=avi \
-  --enable-demuxer=flv \
-  --enable-demuxer=webm \
-  --enable-demuxer=mpegts \
-  --enable-demuxer=mpegps \
-  --enable-demuxer=asf \
-  \
-  --enable-muxer=mp4 \
-  --enable-muxer=mov \
-  --enable-muxer=adts \
-  \
-  --enable-decoder=h264 \
-  --enable-decoder=hevc \
-  --enable-decoder=mpeg4 \
-  --enable-decoder=mpeg2video \
-  --enable-decoder=vp8 \
-  --enable-decoder=vp9 \
-  --enable-decoder=wmv3 \
-  --enable-decoder=msmpeg4v3 \
-  \
-  --enable-decoder=aac \
-  --enable-decoder=mp3 \
-  --enable-decoder=ac3 \
-  --enable-decoder=pcm_s16le \
-  --enable-decoder=vorbis \
-  \
-  --enable-parser=h264 \
-  --enable-parser=hevc \
-  --enable-parser=mpeg4video \
-  --enable-parser=mpegaudio \
-  --enable-parser=aac \
-  \
-  --enable-videotoolbox \
-  --enable-hwaccel=h264_videotoolbox \
-  \
-  --enable-encoder=h264_videotoolbox \
-  --enable-encoder=aac \
-  \
-  --enable-small \
-  --disable-runtime-cpudetect \
-  \
-  --enable-xcframework
+  --disable-i386 \
+  --disable-arm64e
 
 cd ..
--- Scripts/build-macos.sh
+++ Scripts/build-macos.sh
@@ -1,77 +1,72 @@
 #!/bin/bash
 set -e
 
-FFMPEG_VERSION=6.0
-
-git clone --depth 1 --branch v${FFMPEG_VERSION} https://github.com/arthenica/ffmpeg-kit.git
+git clone --depth 1 --branch v6.0 https://github.com/arthenica/ffmpeg-kit.git
 cd ffmpeg-kit
 
-./macos.sh all \
-  --disable-armv7 \
-  --disable-armv7s \
-  \
-  --disable-gpl \
-  --disable-nonfree \
-  --disable-debug \
-  --disable-programs \
-  --disable-doc \
-  --disable-avdevice \
-  --disable-postproc \
-  --disable-network \
-  --disable-autodetect \
-  --disable-everything \
-  \
-  --enable-avcodec \
-  --enable-avformat \
-  --enable-avutil \
-  --enable-swresample \
-  --enable-swscale \
-  \
-  --enable-protocol=file \
-  \
-  --enable-demuxer=mov \
-  --enable-demuxer=matroska \
-  --enable-demuxer=avi \
-  --enable-demuxer=flv \
-  --enable-demuxer=webm \
-  --enable-demuxer=mpegts \
-  --enable-demuxer=mpegps \
-  --enable-demuxer=asf \
-  \
-  --enable-muxer=mp4 \
-  --enable-muxer=mov \
-  --enable-muxer=adts \
-  \
-  --enable-decoder=h264 \
-  --enable-decoder=hevc \
-  --enable-decoder=mpeg4 \
-  --enable-decoder=mpeg2video \
-  --enable-decoder=vp8 \
-  --enable-decoder=vp9 \
-  --enable-decoder=wmv3 \
-  --enable-decoder=msmpeg4v3 \
-  \
-  --enable-decoder=aac \
-  --enable-decoder=mp3 \
-  --enable-decoder=ac3 \
-  --enable-decoder=pcm_s16le \
-  --enable-decoder=vorbis \
-  \
-  --enable-parser=h264 \
-  --enable-parser=hevc \
-  --enable-parser=mpeg4video \
-  --enable-parser=mpegaudio \
-  --enable-parser=aac \
-  \
-  --enable-videotoolbox \
-  --enable-hwaccel=h264_videotoolbox \
-  \
-  --enable-encoder=h264_videotoolbox \
-  --enable-encoder=aac \
-  \
-  --enable-small \
-  --disable-runtime-cpudetect \
-  \
-  --enable-xcframework
+export FFMPEG_CONFIGURE_OPTIONS="\
+--disable-everything \
+--enable-small \
+--disable-programs \
+--disable-doc \
+--disable-debug \
+--disable-network \
+--disable-autodetect \
+\
+--enable-avcodec \
+--enable-avformat \
+--enable-avutil \
+--enable-swresample \
+--enable-swscale \
+\
+--enable-protocol=file \
+\
+--enable-demuxer=mov \
+--enable-demuxer=matroska \
+--enable-demuxer=avi \
+--enable-demuxer=flv \
+--enable-demuxer=webm \
+--enable-demuxer=mpegts \
+--enable-demuxer=mpegps \
+--enable-demuxer=asf \
+\
+--enable-muxer=mp4 \
+--enable-muxer=mov \
+--enable-muxer=adts \
+\
+--enable-decoder=h264 \
+--enable-decoder=hevc \
+--enable-decoder=mpeg4 \
+--enable-decoder=mpeg2video \
+--enable-decoder=vp8 \
+--enable-decoder=vp9 \
+--enable-decoder=wmv3 \
+--enable-decoder=msmpeg4v3 \
+\
+--enable-decoder=aac \
+--enable-decoder=mp3 \
+--enable-decoder=ac3 \
+--enable-decoder=pcm_s16le \
+--enable-decoder=vorbis \
+\
+--enable-parser=h264 \
+--enable-parser=hevc \
+--enable-parser=mpeg4video \
+--enable-parser=mpegaudio \
+--enable-parser=aac \
+\
+--enable-encoder=h264_videotoolbox \
+--enable-encoder=aac \
+\
+--enable-hwaccel=h264_videotoolbox \
+"
+
+./macos.sh \
+  --xcframework \
+  --enable-macos-videotoolbox \
+  --enable-macos-audiotoolbox \
+  --enable-macos-zlib \
+  --disable-arm64 \
+  --disable-x86-64
 
 cd ..
