4517d4e4e40f548b904d2a8bc793b92825823003
liujianping <gitdig.com@gmail.com>
2026-02-13 15:54:25 +0800

    add files

---
+++ Scripts/build-macos.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+set -e
+
+FFMPEG_VERSION=6.0
+
+git clone --depth 1 --branch v${FFMPEG_VERSION} https://github.com/arthenica/ffmpeg-kit.git
+cd ffmpeg-kit
+
+./macos.sh \
+  --disable-armv7 \
+  --disable-armv7s \
+  \
+  --disable-gpl \
+  --disable-nonfree \
+  --disable-debug \
+  --disable-programs \
+  --disable-doc \
+  --disable-avdevice \
+  --disable-postproc \
+  --disable-network \
+  --disable-autodetect \
+  --disable-everything \
+  \
+  --enable-avcodec \
+  --enable-avformat \
+  --enable-avutil \
+  --enable-swresample \
+  --enable-swscale \
+  \
+  --enable-protocol=file \
+  \
+  --enable-demuxer=mov \
+  --enable-demuxer=matroska \
+  --enable-demuxer=avi \
+  --enable-demuxer=flv \
+  --enable-demuxer=webm \
+  --enable-demuxer=mpegts \
+  --enable-demuxer=mpegps \
+  --enable-demuxer=asf \
+  \
+  --enable-muxer=mp4 \
+  --enable-muxer=mov \
+  --enable-muxer=adts \
+  \
+  --enable-decoder=h264 \
+  --enable-decoder=hevc \
+  --enable-decoder=mpeg4 \
+  --enable-decoder=mpeg2video \
+  --enable-decoder=vp8 \
+  --enable-decoder=vp9 \
+  --enable-decoder=wmv3 \
+  --enable-decoder=msmpeg4v3 \
+  \
+  --enable-decoder=aac \
+  --enable-decoder=mp3 \
+  --enable-decoder=ac3 \
+  --enable-decoder=pcm_s16le \
+  --enable-decoder=vorbis \
+  \
+  --enable-parser=h264 \
+  --enable-parser=hevc \
+  --enable-parser=mpeg4video \
+  --enable-parser=mpegaudio \
+  --enable-parser=aac \
+  \
+  --enable-videotoolbox \
+  --enable-hwaccel=h264_videotoolbox \
+  \
+  --enable-encoder=h264_videotoolbox \
+  --enable-encoder=aac \
+  \
+  --enable-small \
+  --disable-runtime-cpudetect \
+  \
+  --enable-xcframework
+
+cd ..
---
+++ Scripts/package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+mkdir -p artifacts
+
+cp -R ffmpeg-kit/prebuilt/apple-ios-xcframework/*.xcframework artifacts/ffmpegkit-ios.xcframework
+cp -R ffmpeg-kit/prebuilt/apple-macos-xcframework/*.xcframework artifacts/ffmpegkit-macos.xcframework
+
+cd artifacts
+
+zip -r ffmpegkit-ios.zip ffmpegkit-ios.xcframework
+zip -r ffmpegkit-macos.zip ffmpegkit-macos.xcframework
+
+IOS_CHECKSUM=$(shasum -a 256 ffmpegkit-ios.zip | awk '{print $1}')
+MAC_CHECKSUM=$(shasum -a 256 ffmpegkit-macos.zip | awk '{print $1}')
+
+echo "IOS_CHECKSUM=$IOS_CHECKSUM" >> $GITHUB_ENV
+echo "MAC_CHECKSUM=$MAC_CHECKSUM" >> $GITHUB_ENV
+
+cd ..
